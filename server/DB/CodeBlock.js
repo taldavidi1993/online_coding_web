d2eb04a534b39970bde9cbe15176997bc110e81d
taldavidi1993 <taldavidi1993@gmail.com>
2023-07-31 16:33:35 +0300

    creating schema for users and codeblocks

---
+++ server/DB/CodeBlock.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const CodeBlockSchema = new mongoose.Schema()(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const CodeBlock = mongoose.model('CodeBlock', CodeBlockSchema);
+export { CodeBlock };
---
+++ server/DB/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema()(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 4,
+    },
+    isAdmin: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+export { User };
