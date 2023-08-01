d08829c245ec6cce40b53b029969e8f5985cf780
taldavidi1993 <taldavidi1993@gmail.com>
2023-08-01 18:15:26 +0300

    codeblock functions
    find all code block
    find by id
    and change codeblocjs name

---
+++ server/controller/codeblock.js
@@ -0,0 +1,33 @@
+const { CodeBlock } = require('../db/CodeBlock');
+
+exports.findAllCodeBlocks = async (req, res, next) => {
+  try {
+    console.log('in');
+    const codeBlock = await CodeBlock.find().select({ title: 1, name: 1 });
+
+    res.send(codeBlock);
+  } catch {}
+};
+
+exports.findOneCodeBlock = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const codeBlock = await CodeBlock.findById(id).select({
+      content: 1,
+      _id: 0,
+    });
+    res.send(codeBlock).status(200);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.changeCodeBlockName = async (req, res, next) => {
+  try {
+    const { id, newName } = req.body;
+    await CodeBlock.findOneAndUpdate({ _id: id }, { name: newName });
+    res.send('name of code block changed');
+  } catch (err) {
+    console.log(err);
+  }
+};
