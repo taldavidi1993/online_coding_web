992b958da287b61840458ba84c2639f67e9733bd
taldavidi1993 <taldavidi1993@gmail.com>
2023-08-03 19:27:27 +0300

    creating routes foreach code block

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,46 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import CodeBlockPage from './CodeBlockPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
-  useEffect(() => {});
-  return <div className="App">app</div>;
+  const [codeBlocks, setCodeBlocks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/findAllCodeBlocks')
+      .then(res => {
+        setCodeBlocks(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Routes>
+            {codeBlocks &&
+              codeBlocks.map(block => {
+                console.log(block);
+                return (
+                  <Route
+                    key={block._id}
+                    // path={`/${block.name}`}
+                    path={`/${block._id}`}
+                    // path={`${block.title.replace(/\s+/g, '-')}/${block._id}`}
+                    element={<CodeBlockPage block={block} />}
+                  />
+                );
+              })}
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
